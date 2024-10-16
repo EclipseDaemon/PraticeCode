9afc57215d7046cd6678b7e48b5b834634d47d13
EclipseDaemon <rajvardhanp15@gmail.com>
2024-10-16 12:29:12 +0530

    new branch commit

--- jsPratice.js
+++ jsPratice.js
@@ -1,7 +1,7 @@
 function reverse(){
     const word = prompt('Enter a word: ');
     const reversed = word.split('').reverse().join('');
-    alert(`Your Palidrome is ${reversed}`);
+    alert(`Palidrome is ${reversed}`);
 }
 
 reverse();
