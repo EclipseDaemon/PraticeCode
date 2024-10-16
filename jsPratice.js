2c95d9519526eb4dffb1222cb7e3a7593993d48a
Rajvardhan Pawar <99598319+RajvardhanP@users.noreply.github.com>
2024-10-16 17:08:01 +0530

    consolelogAdded

--- jsPratice.js
+++ jsPratice.js
@@ -2,6 +2,7 @@ function reverse(){
     const word = prompt('Enter a word: ');
     const reversed = word.split('').reverse().join('');
     alert(`Palidrome is ${reversed}`);
+    console.log(word);
 }
 
 reverse();
